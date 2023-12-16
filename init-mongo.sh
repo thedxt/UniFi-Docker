ac143e8964c6eee070f1343e756b34c4154f5892
Daniel Keer <4249262+thedxt@users.noreply.github.com>
2023-12-16 10:09:59 -0700

    Create init-mongo.sh

---
+++ init-mongo.sh
@@ -0,0 +1,20 @@
+set -e
+
+mongo <<EOF
+db.getSiblingDB('$MONGO_DBNAME').createUser({
+  user: '$MONGO_USER',
+  pwd: '$MONGO_PASS',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_DBNAME'
+  }]
+})
+db.getSiblingDB('$MONGO_DBNAMESTAT').createUser({
+  user: '$MONGO_USER',
+  pwd: '$MONGO_PASS',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_DBNAMESTAT'
+  }]
+})
+EOF
